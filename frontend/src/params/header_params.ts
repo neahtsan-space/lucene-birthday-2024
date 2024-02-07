b5411f1207d318f94a58b77f485befc831434cee
Thaenthai <thaenthai124@gmail.com>
2024-02-08 00:31:09 +0700

    [ADD] footer_params, header_params, and navbar_params script

---
+++ frontend/src/params/header_params.ts
@@ -0,0 +1,19 @@
+const topic = "Lucene Birthday 2024"
+
+const button1_EN = "AboutPolygon"
+const button2_EN = "Projects"
+const button3_EN = "About"
+const button4_EN = "Services"
+const button5_EN = "Contacts"
+
+const button1_TH = "เกี่ยวกับ PLG"
+const button2_TH = "โครงการ"
+const button3_TH = "เกี่ยวกับ"
+const button4_TH = "บริการ"
+const button5_TH = "ติดต่อ"
+
+const button_ALL_TH = [button1_TH, button2_TH, button3_TH, button4_TH, button5_TH]
+const button_ALL_EN = [button1_EN, button2_EN, button3_EN, button4_EN, button5_EN]
+
+const button_TH = 'TH'
+const button_EN = 'EN'
