62ee051bc8bf1cc9ebf06faaf388d90c00ee5d0f
Thaenthai <thaenthai124@gmail.com>
2024-02-09 22:27:04 +0700

    [ADD] WishCommand to page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
-import ViewAllWishcard from './view-all-wishcard/page';
 import Header from '../components/header';
 import DashBoard from '@/components/dashboard';
+import WishCommand from '@/components/wishcommand';
 import RecentWish from '@/components/recentWish';
 import Footer from '../components/footer';
 import styles from './page.module.css';
@@ -20,17 +20,9 @@ const Home: React.FC = () => {
             <DashBoard />
           </div>
           <div className={styles.box2}>
-            <span className={styles.text}>{body.textbody}</span>
-            <hr className={styles.line} /> {/* Horizontal line */}
-            <div className={styles.buttoncontainer}>
-            <a href={body.body_link1} className="body-link">
-            <button className="custom-button">{body.body_button1}</button></a>
-            <a href={body.body_link2} className="body-link">
-            <button className="custom-button">{body.body_button2}</button></a>
-            <a href={body.body_link3} className="body-link">
-            <button className="custom-button">{body.body_button3}</button></a>
+            <div className={styles.box2Title}>
+              <WishCommand />
             </div>
-            <span className={styles.text2}>{body.wish}</span>
           </div>
         </div>
         <div className={styles.fullWidthBox}>
