b2920e3b48a8e3bdae594b477dc6e25908f6c9f3
Thaenthai <thaenthai124@gmail.com>
2024-02-10 02:16:47 +0700

    [UPDATE] Mock data to present interface

--- frontend/src/app/view-all-wishcard/cardtable.tsx
+++ frontend/src/app/view-all-wishcard/cardtable.tsx
@@ -3,41 +3,11 @@ import '@/css/header.css';
 import { Table } from 'antd';
 import WishCardTemplate from '@/utils/card'; // Import wishCardTemplate component
 import wishCard from '@/interfaces/IWishcard'; // Assuming you have defined IWishCard interface
+import { mock_data } from '@/params/mockdata_params'; // Import mock data
 
 interface CardTableProps {
   data: typeof mock_data; // Replace with your actual data type
 }
-
-
-const mock_data = [
-  {
-    "_id": "65bfe6c579c59ae90170b0e2",
-    "name": "neaht",
-    "wish": "แฮปๆนะจ๊ะสาวน้อย",
-    "time": "05 Feb 2024 02:34 GMT+7",
-    "picture": "1",
-    "cardNumber": 1,
-    "__v": 0
-  },
-  {
-    "_id": "65c3e36518f1fa737a5f4038",
-    "name": "neaht3",
-    "wish": "แฮปๆนะจ๊ะสาวน้อย ๆ ๆ ๆ ๆ ๆ",
-    "time": "08 Feb 2024 03:09 GMT+7",
-    "picture": "1",
-    "cardNumber": 2,
-    "__v": 0
-  },
-  {
-    "_id": "65c3e36a18f1fa737a5f403c",
-    "name": "neaht4",
-    "wish": "แฮปๆนะจ๊ะสาวน้อย ๆ ๆ ๆ ๆ ๆ",
-    "time": "08 Feb 2024 03:09 GMT+7",
-    "picture": "1",
-    "cardNumber": 3,
-    "__v": 0
-  }
-]
 
 const CardTable: React.FC<CardTableProps> = ({ data: tableData }) => {
   if (!mock_data || mock_data.length === 0) {
--- frontend/src/components/view-all-wish.tsx
+++ frontend/src/components/view-all-wish.tsx
@@ -1,39 +1,9 @@
 import React from 'react';
 import CardTable from "@/app/view-all-wishcard/cardtable";
-
+import { mock_data } from "@/params/mockdata_params";
 
 
 const WishTable: React.FC = () => {
-
-    const mock_data = [
-    {
-      "_id": "65bfe6c579c59ae90170b0e2",
-      "name": "neaht",
-      "wish": "แฮปๆนะจ๊ะสาวน้อย",
-      "time": "05 Feb 2024 02:34 GMT+7",
-      "picture": "1",
-      "cardNumber": 1,
-      "__v": 0
-    },
-    {
-      "_id": "65c3e36518f1fa737a5f4038",
-      "name": "neaht3",
-      "wish": "แฮปๆนะจ๊ะสาวน้อย ๆ ๆ ๆ ๆ ๆ",
-      "time": "08 Feb 2024 03:09 GMT+7",
-      "picture": "1",
-      "cardNumber": 2,
-      "__v": 0
-    },
-    {
-      "_id": "65c3e36a18f1fa737a5f403c",
-      "name": "neaht4",
-      "wish": "แฮปๆนะจ๊ะสาวน้อย ๆ ๆ ๆ ๆ ๆ",
-      "time": "08 Feb 2024 03:09 GMT+7",
-      "picture": "1",
-      "cardNumber": 3,
-      "__v": 0
-    }
-  ]
 
     return (
       <div>
