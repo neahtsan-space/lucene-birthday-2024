7678c367f9ee37c187de500742be116293dc5a2e
Thaenthai <thaenthai124@gmail.com>
2024-02-09 15:40:25 +0700

    [FIX] Footer params & CSS

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import '@/css/footer.css';
 import * as FooterParams from '@/params/footer_params';
+import Image from 'next/image';
 
 const Footer: React.FC = () => {
   return (
     <footer>
       <div className="footer">
-        <div className='.footer-text-container'>      
-        <a href={FooterParams.imglink1_URL} className="image-link">
-        <img src={FooterParams.imgsrc1} className="custom-img" alt="Discord" /></a>
-        <a href={FooterParams.imglink2_URL} className="image-link">
-        <img src={FooterParams.imgsrc2} className="custom-img" alt="Discord" /></a>
-        <a href={FooterParams.imglink3_URL} className="image-link">
-        <img src={FooterParams.imgsrc3} className="custom-img" alt="Discord" /></a>
+        <div className='footer-text-container'>      
+          <a href={FooterParams.imglink1_URL} className="discord">
+            <Image src={FooterParams.imgsrc1} width={40} height={40} className="custom-img" alt="Discord" /></a>
+            <p className='footer-discord-name'>{FooterParams.DISCORD_NAME}</p>
+          <p className='footer-follow-talent'>{FooterParams.FOLLOW}
+            <span className="footer-name-underline">{FooterParams.TALENT_NAME}</span>
+            {FooterParams.FOLLOW_END}
+          </p>
+          <a href={FooterParams.imglink2_URL} className="youtube">
+            <Image src={FooterParams.imgsrc2} width={40} height={40} className="custom-img" alt="Youtube" /></a>
+          <a href={FooterParams.imglink3_URL} className="x_twitter">
+            <Image src={FooterParams.imgsrc3} width={40} height={40} className="custom-img" alt="X_logo" /></a>
         </div>
         <div></div>
       </div>
