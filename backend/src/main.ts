95bf6f1433911eef82965af3a3718a613d2d7fd0
Thaenthai <thaenthai124@gmail.com>
2024-02-10 01:00:17 +0700

    [ADD] Implement rate limit to application

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
   app.enableCors({
     origin: ['http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+  // Apply rate limiting
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+      standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+      legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    }),
+  );
+
   await app.listen(3080);
   Logger.log('Running on port 3080', 'NestApplication');
 }
+
 bootstrap();
