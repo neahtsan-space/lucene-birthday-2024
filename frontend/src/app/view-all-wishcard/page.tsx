631196f849967542ad7150969f03fec528188dc1
Thaenthai <thaenthai124@gmail.com>
2024-02-10 16:57:55 +0700

    [ADD] Loading Screen to view-all-wishcard page

--- frontend/src/app/view-all-wishcard/page.tsx
+++ frontend/src/app/view-all-wishcard/page.tsx
@@ -2,14 +2,16 @@
 import Header2 from "@/components/header2";
 import Footer from "@/components/footer";
 import './page.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import WishTable from "@/components/view-all-wish";
 
 const ViewAllWishcard: React.FC = () => {
     return (
       <div>
         <Header2 />
-        <WishTable />
+        <Suspense fallback={<div>Loading...</div>}>
+          <WishTable />
+        </Suspense>
         <Footer />
       </div>
     );
