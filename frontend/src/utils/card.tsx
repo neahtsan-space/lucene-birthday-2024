e97f3778a65a1daac15bb0642fcedac93fc7aaf3
Thaenthai <thaenthai124@gmail.com>
2024-02-10 14:50:15 +0700

    [ADD] Improve the card template for clicking and opening the modal of the larger card

--- frontend/src/utils/card.tsx
+++ frontend/src/utils/card.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import IWishCard from '@/interfaces/IWishcard';
 import { STICKER_7 } from '@/params/card_params';
-
-
-import { Card } from 'antd';
+import { Card, Modal } from 'antd';
 
 const { Meta } = Card;
 
 const WishCardTemplate: React.FC<{wishCard: IWishCard}> = ({wishCard}) => {
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
     return (
         <div>
-        <Card
-            hoverable
-            style={{ margin: 20,width: 300 }}
-            cover={<Image alt="example" src={STICKER_7} width={40} height={400} sizes='100vw' />}
-        >
-            <Meta title={`${wishCard.name} #${wishCard.cardNumber}`} description={wishCard.wish} />
-        </Card>
+            {/* The original card that opens the modal on click */}
+            <Card
+                hoverable
+                style={{ margin: 20, width: 300 }}
+                cover={<Image alt="example" src={STICKER_7} width={40} height={400} layout='responsive' />}
+                onClick={showModal}
+            >
+                <Meta title={`${wishCard.name} #${wishCard.cardNumber}`} description={wishCard.wish} />
+            </Card>
+            
+            {/* The modal that displays the larger card */}
+            <Modal
+                open={isModalVisible}
+                footer={null} // Hides the default footer
+                onCancel={handleCancel}
+                width={600} // Adjust the size of the modal as needed
+            >
+                <Card
+                    hoverable
+                    style={{ width: '100%' }} // Adjust the card size within the modal as needed
+                    cover={<Image alt="example" src={STICKER_7} width={40} height={400} layout='responsive' />}
+                >
+                    <Meta title={`${wishCard.name} #${wishCard.cardNumber}`} description={wishCard.wish} />
+                </Card>
+            </Modal>
         </div>
     );
     }
