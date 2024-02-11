79d865dec7e3eab216aa2c27e4ef6a13634b410a
Thaenthai <thaenthai124@gmail.com>
2024-02-12 03:30:32 +0700

    [REMOVE] Rate limit for development purpose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,21 +6,10 @@ import rateLimit from 'express-rate-limit';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
   app.enableCors({
     methods: 'GET,HEAD,POST,OPTIONS',
     credentials: false,
   });
-
-  // Apply rate limiting
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-      standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-      legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-    }),
-  );
 
   await app.listen(3080);
   Logger.log('Running on port 3080', 'NestApplication');
