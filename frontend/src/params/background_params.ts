0e843fa1c86eeb92ff2e87d53c9f78debbfbd3b9
Thaenthai <thaenthai124@gmail.com>
2024-02-13 03:13:26 +0700

    [ADD] Migrate from ANTD carousel to Swiper carousel

--- frontend/src/params/background_params.ts
+++ frontend/src/params/background_params.ts
@@ -3,6 +3,6 @@
 export const RECENT_WISH_BG = '#A5DAE0' // turqoise (maybe)
 export const WISHCOMMAND_BG = '#A5DAE0'
 export const VIEWALLWISH_BG = '#A5DAE0'
-export const DASHBOARD_BG = '#0078F0' // grey yellow (maybe)
-export const DASHBOARD_TITLE_BG = '#00B2EC'
-export const DASHBOARD_DESC_BG = '#D8D0B9'
+export const DASHBOARD_BG = '#D8D0B9'
+export const DASHBOARD_TITLE_BG = '#0078F0'
+export const DASHBOARD_DESC_BG = '#00B2EC'
--- frontend/src/utils/carousel.tsx
+++ frontend/src/utils/carousel.tsx
@@ -1,63 +1,69 @@
-import React, { useRef, useState } from 'react';
-import { Carousel, Button } from 'antd';
-import { LeftOutlined, RightOutlined } from '@ant-design/icons';
-import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+import Image from 'next/legacy/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation'; 
+import 'swiper/css/effect-fade';
+import { Pagination, Navigation, EffectFade, Autoplay } from 'swiper/modules';
 import '@/css/carousel.css';
 import { dashboardImages, DASHBOARD_TITLE } from '@/params/dashboard_param';
 import { DASHBOARD_BG, DASHBOARD_DESC_BG, DASHBOARD_TITLE_BG } from '@/params/background_params';
 
-
-
-// test dashboard carousel
-
 const DashboardCarousel: React.FC = () => {
-
   const [currentDescription, setCurrentDescription] = useState('');
-  const carouselRef = useRef<any>(null);
 
-  const goToNextSlide = () => carouselRef.current?.next();
-  const goToPrevSlide = () => carouselRef.current?.prev();
-
-  useState(() => {
+  useEffect(() => {
     setCurrentDescription(dashboardImages[0].description);
-  });
+  }, []);
 
-  const handleAfterChange = (currentSlide: number) => {
-    setCurrentDescription(dashboardImages[currentSlide].description);
-  };
-  
-  const imageStyle: React.CSSProperties = {
-    width: 'auto',
-    height: 'auto',
-    overflow: 'hidden',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'relative',
-    margin: 'auto',
-    maxHeight: '45vh',
-    backgroundColor: DASHBOARD_BG,
+  const handleAfterChange = (swiper: any) => {
+    setCurrentDescription(dashboardImages[swiper.realIndex].description);
   };
 
   return (
-    <div style={{backgroundColor: DASHBOARD_BG}}>
-      <div className="carousel-description" style={{backgroundColor: DASHBOARD_TITLE_BG}}>{DASHBOARD_TITLE}</div>
-      <Carousel autoplay ref={carouselRef} afterChange={handleAfterChange} className="carousel">
+    <div>
+      <div style={{backgroundColor: DASHBOARD_TITLE_BG, display: 'flex', alignItems: 'center', justifyContent: 'center', height: '2.5vw'}}>{DASHBOARD_TITLE}</div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        effect="fade"
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          type: 'progressbar',
+          clickable: true,
+        }}
+        navigation={true} 
+        modules={[Pagination, Navigation, EffectFade, Autoplay]}
+        className="mySwiper"
+        onSlideChange={handleAfterChange}
+      >
         {dashboardImages.map((image, index) => (
-          <div key={index} className="carousel-container" style={imageStyle}>
-            <Image src={image.src} alt={image.alt} width={0} height={0} sizes="100vw" style={imageStyle} 
-            priority={index === 0}
-            loading={index === 0 ? 'eager' : 'lazy'}/>
-          </div>
+          <SwiperSlide key={index}>
+            <div style={{
+              position: 'relative',
+              width: '50vw',
+              height: '25vw',
+              backgroundColor: DASHBOARD_BG
+            }}>
+              <Image
+                src={image.src}
+                alt={image.alt}
+                layout='fill'
+                objectFit='contain'
+                sizes='(max-width: 600px) 100vw, (max-width: 1024px) 25vw, 1024px'
+              />
+            </div>
+          </SwiperSlide>
         ))}
-      </Carousel>
-      <Button type="text" onClick={goToPrevSlide} style={{ position: 'absolute', top: '39vh', left: '10vh', zIndex: 2, transform: 'translateY(-50%)' }}>
-          <LeftOutlined style={{ fontSize: '2rem', color: 'white' }} />
-      </Button>
-      <Button type="text" onClick={goToNextSlide} style={{ position: 'absolute', top: '39vh', right: '110vh', zIndex: 2, transform: 'translateY(-50%)' }}>
-          <RightOutlined style={{ fontSize: '2rem', color: 'white' }} />
-      </Button>
-      <div className="carousel-description" style={{backgroundColor: DASHBOARD_DESC_BG}}>{currentDescription}</div>
+      </Swiper>
+      <div style={{ backgroundColor: DASHBOARD_DESC_BG, display: 'flex', alignItems: 'center', justifyContent: 'center', height: '2.5vw' }}>
+        {currentDescription}
+      </div>
     </div>
   );
 };
