ee312833ef2714f88346345819a64d242f6e8a7c
Thaenthai <thaenthai124@gmail.com>
2024-02-15 23:23:23 +0700

    [FIX] PopCat Count

--- frontend/src/app/popcat/popcat.tsx
+++ frontend/src/app/popcat/popcat.tsx
@@ -1,9 +1,8 @@
 'use client';
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import '@/app/popcat/style.css';
 import Image from 'next/image';
-import Aud from './play';
 
 const PopCatApp: React.FC = () => {
   const [popcatSrc, setPopcatSrc] = useState<string>('/popcat/close.png');
@@ -11,26 +10,42 @@ const PopCatApp: React.FC = () => {
   const openMouthImg: string = '/popcat/open.png';
   const closeMouthImg: string = '/popcat/close.png';
 
-  // State to store audio objects
   const [openMouthSound, setOpenMouthSound] = useState<HTMLAudioElement | null>(null);
   const [closeMouthSound, setCloseMouthSound] = useState<HTMLAudioElement | null>(null);
 
   useEffect(() => {
-    // Initialize audio elements after the component has mounted
     setOpenMouthSound(new Audio('/popcat/sound/sound-open.mp3'));
     setCloseMouthSound(new Audio('/popcat/sound/sound-close.mp3'));
   }, []);
 
-  const toggleMouth = useCallback((isOpen: boolean): void => {
-    setPopcatSrc(isOpen ? openMouthImg : closeMouthImg);
+  const openMouth = (): void => {
+    setPopcatSrc(openMouthImg);
     setPopCount((currentCount) => currentCount + 1);
+    openMouthSound?.play();
+  };
 
-    if (isOpen) {
-      openMouthSound?.play();
-    } else {
-      closeMouthSound?.play();
-    }
-  }, [openMouthImg, closeMouthImg, openMouthSound, closeMouthSound]);
+  const closeMouth = (): void => {
+    setPopcatSrc(closeMouthImg);
+    closeMouthSound?.play();
+  };
+
+  const handleMouseDown = (): void => {
+    openMouth();
+  };
+
+  const handleMouseUp = (): void => {
+    closeMouth();
+  };
+
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    openMouth();
+  };
+
+  const handleTouchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    closeMouth();
+  };
 
   return (
     <>
@@ -40,22 +55,14 @@ const PopCatApp: React.FC = () => {
       </Head>
       <div className='wrapper'>
         <div
-          onMouseDown={() => toggleMouth(true)}
-          onMouseUp={() => toggleMouth(false)}
-          onTouchStart={(e) => {
-            e.preventDefault();
-            toggleMouth(true);
-          }}
-          onTouchEnd={(e) => {
-            e.preventDefault();
-            toggleMouth(false);
-          }}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onTouchStart={handleTouchStart}
+          onTouchEnd={handleTouchEnd}
           style={{ cursor: 'pointer', width: '50vw', height: '50vw', position: 'relative' }}
         >
           <Image src={popcatSrc} alt="Pop Cat" layout='fill' objectFit='contain' />
         </div>
-        <p><Aud></Aud>
-        </p>
         <p>Pop Count: {popCount}</p>
       </div>
     </>
