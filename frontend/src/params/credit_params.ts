eb0c6c4f88f3e0ca41fbb273bb66c8a4b56919c0
razermanth <razermanth@gmail.com>
2024-02-24 03:05:01 +0700

    [FIX] credit link

--- frontend/src/params/credit_params.ts
+++ frontend/src/params/credit_params.ts
@@ -1,8 +1,8 @@
 export const credits: { name: string; description: string; link: string }[] = [
-    { name: 'Neaht', description: 'Project Developer', link: 'https://twitter.com/NeahtSan2nd' },
-    { name: 'Razerman', description: 'Project Developer', link: 'razerman.html' },
+    { name: 'Neaht', description: 'Developer', link: 'https://twitter.com/NeahtSan2nd' },
+    { name: 'Razerman', description: 'Developer', link: 'razerman.html' },
     { name: 'CV_FSan', description: 'Artwork Provider', link: 'https://twitter.com/cvfsan' },
     { name: 'สิราวิ้จจะบึ', description: 'Project Inspiration', link: 'https://twitter.com/SierraB_Siravij' },
     /*{ name: 'GPT-KUN ft. COPILOT-SAN', description: 'KYA', link: 'gpt_kun.html' },*/
-    { name: 'Polygon Project', description: 'Project Approval', link: 'copilot_san.html' },
+    { name: 'Polygon Project', description: 'Project Approval', link: 'https://www.plg.tv/' },
 ];
